64b4cc3ebf4e0ec7077b9f724d1c0cbe38fcbcb7
Maximiliano Ovejak <maximiliano.ovejak@gmail.com>
2023-01-12 08:24:05 -0500

    cambios de nav y otros

--- components/NavBar/Nav.jsx
+++ components/NavBar/Nav.jsx
@@ -19,7 +19,7 @@ const Nav = () => {
           className={styles.logoDark}
         />
 
-        <ul>
+        <ul className={styles.deskNav}>
           <li>
             <div></div>
             <a href="#home">Home</a>
@@ -40,6 +40,29 @@ const Nav = () => {
             <div></div>
             <a href="#contact">Contact</a>
           </li>
+        </ul>
+
+        <ul className={styles.mobileNav}>
+          <div className={styles.overlay}></div>
+
+          <div className={styles.links}>
+
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#work">Work</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </div>
         </ul>
     </nav>
   )
